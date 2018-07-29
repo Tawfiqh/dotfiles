3cb19bfc7076c49d0f6bfb6a65bfb059f3a04dca
Tawfiq Hamid <Tawfiq.Hamid@gmail.com>
2018-07-29 22:24:39 +0100

    Added fish setup files from old Macbook.

--- config.fish
+++ config.fish
@@ -1,1 +1,100 @@
-/Users/tawfiq/.config/fish/config.fish
+alias ll="ls -Gap"
+
+alias lld="ls -d ./*/"
+
+function gitpullprojects
+  echo "Put a list of directories here to iterate through and run 'git pull'"
+end
+
+
+function cs
+	 cd $argv; ll; printf "\n"; pwd;
+end
+
+function tgre
+	 grep -ir $argv *;
+end
+
+
+function tgres
+	 grep -iIrF $argv * --exclude-dir log --exclude-dir tmp --exclude-dir _site --exclude-dir node_modules --exclude-dir vendor;
+end
+
+function ltgres
+	 tgres -l $argv
+end
+
+function oltgres
+	 ltgres $argv |xargs open
+end
+
+
+alias otgres="oltgres"
+
+function otgre
+	 tgre -l $argv |xargs open;
+end
+
+alias otgrex="otgres"
+
+function tfind
+	 find . -iname "*$argv*";
+end
+
+function otfind
+	 tfind "$argv" |xargs open ;
+end
+
+function dhikr
+  set n $argv[1]
+
+  if test -z $n
+    set n 3
+  end
+
+
+  for x in (seq $n)
+    say "Ul-lar  Hu" -r (math "((40 * $x) % 350)+ 200")
+  end
+
+end
+
+alias tip="ipconfig getifaddr en1| awk '{print \"http:\/\/\" \$1 \":3000/\"}'|pbcopy;pbpaste;echo \"[Copied to clipboard]\""
+
+function otouch --argument file app
+
+ if test -z $file
+  return
+ end
+
+ touch $file;
+
+ if test -z $app
+  open $file
+  return
+ end
+
+ open -a $app $file
+
+
+end
+
+set -Ux EDITOR emacs
+
+
+
+#CONFIG FOR PATH STUFF
+
+#set -x PATH $PATH /Users/Tawfiq/.rbenv/shims /Users/Tawfiq/.rbenv/  #To find where other ruby stuff is installed run `gem env`
+#set -x PATH $PATH /usr/local/lib/ruby/gems/2.4.0 /Users/Tawfiq/.gem/ruby/ ~/.gem/ruby/2.0.0 rbenv global 2.3.0
+#set -x PATH $PATH  /Library/Frameworks/Python.framework/Versions/3.5/bin
+
+#Some QMake thing from QT5, required for Capybara
+set -x PATH $PATH /usr/local/Cellar/qt@5.5/5.5.1_1/bin
+#had to go to ./usr/local/Cellar and run:
+# $ tfind qmake | grep "make\$"
+#in order to find which bin folder to add to the path for QMake.
+
+
+#Some rbenv thing
+status --is-interactive; and source (rbenv init -|psub)
--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,1 +1,51 @@
-/Users/tawfiq/.config/fish/functions/fish_prompt.fish
+function fish_prompt
+	set -l __last_command_exit_status $status
+
+    if not set -q -g __fish_robbyrussell_functions_defined
+        set -g __fish_robbyrussell_functions_defined
+        function _git_branch_name
+            set -l branch (git symbolic-ref --quiet HEAD ^/dev/null)
+            if set -q branch[1]
+                echo (string replace -r '^refs/heads/' '' $branch)
+            else
+                echo (git rev-parse --short HEAD ^/dev/null)
+            end
+        end
+
+        function _is_git_repo
+            type -q git
+            or return 1
+            git status -s >/dev/null ^/dev/null
+        end
+
+        function _repo_branch_name
+            eval "_$argv[1]_branch_name"
+        end
+
+        function _repo_type
+            if _is_git_repo
+                echo 'git'
+            end
+        end
+    end
+
+    set -l cyan (set_color -o cyan)
+    set -l yellow (set_color -o yellow)
+    set -l red (set_color -o red)
+    set -l green (set_color -o green)
+    set -l blue (set_color -o blue)
+    set -l normal (set_color normal)
+
+    set -l orange (set_color -o f97a02)
+
+    set -l cwd (set_color $fish_color_normal) [(prompt_pwd)]
+
+    set -l repo_type (_repo_type)
+    if [ $repo_type ]
+        set -l repo_branch $cyan(_repo_branch_name $repo_type)
+        set repo_info "$red $repo_type:($repo_branch$red)"
+
+    end
+
+    echo -n -s $cwd $repo_info $normal ' $ '
+end
